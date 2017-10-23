25789198672ef7854e704f4ef53d80729170d0ac
BaptisteHiggs <16fogterminator@gmail.com>
2017-10-23 23:18:55 +1100

    working serva

--- PyServer.py
+++ PyServer.py
@@ -3,6 +3,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
+valCount = 0
 
 import serial
 
@@ -26,12 +27,21 @@ def test():
     #from_arduino = int(ser.readline())
     #print from_arduino
     #return str(from_arduino)
-    print "hey I'm working"
+    global valCount
+    print "hey I'm workin"
     # return str("woah I'm working")
-    #ser = serial.Serial('COM3', 9600)
-    #from_arduino = ser.readline()
-    # print from_arduino
-    return render_template('bikestat.html', num_people="14")
+    ser = serial.Serial('COM3', 9600)
+    from_arduino = str(ser.readline())
+    try:
+        if '1' in from_arduino:
+            print 'ooooh'
+            valCount += 1
+            print valCount
+    except Exception:
+        print 'oopsies'
+    print 'wowsers'
+    print valCount
+    return render_template('bikestat.html', num_people=str(valCount))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
